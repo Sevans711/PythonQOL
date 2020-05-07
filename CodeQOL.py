aa5dd0ce96aee82c81c917ebd65dd579d8c76546
Samuel Evans <Sevans@bu.edu>
2020-05-07 18:46:19 -0400

    Adds strmatches for filtering strings to be kept, based on if they strmatch something in the 'keep' list and nothing in the 'discard' list. Used in PlotQOL for simplifying dictplot.

--- CodeQOL.py
+++ CodeQOL.py
@@ -55,11 +55,12 @@ def pop_from_kwargs(popthis, kwargs, default=None):
     popped = kwargs.pop(popthis, default)
     return (popped, kwargs)
 
-def strmatch(x, str_to_match):
-    """returns whether x 'matches' str_to_match.
+def strmatch(x, y):
+    """returns whether x 'matches' y.
     
     A 'match' is equality, or leading/trailing *'s to stand in for anything.
     Cannot test for strings with non-special *'s in them.
+    Only one of x & y is permitted to have '*'s in it. Else, returns -1.
     
     Examples
     --------------
@@ -71,16 +72,70 @@ def strmatch(x, str_to_match):
     el* doesnt match hello
     *el doesnt match hello
     """
+    if '*' in y: #then swap x & y.
+        if '*' in x: return -1
+        else: return strmatch(y, x)
     if x[0]=='*':
         if x[-1]=='*':
-            return (x[1:-1] in str_to_match)
+            return (x[1:-1] in y)
         else:
-            return str_to_match.endswith(x[1:])
+            return y.endswith(x[1:])
     elif x[-1]=='*':
-        return str_to_match.startswith(x[:-1])
+        return y.startswith(x[:-1])
     else:
-        return x==str_to_match
+        return x==y
 
+def strmatches(x, keep=None, discard=None):
+    """returns which strings in x 'match' anything in keep, and nothing in discard.
+    
+    A 'match' is equality, or leading/trailing *'s to stand in for anything.
+    Cannot test for strings with non-special *'s in them.
+    
+    For each string s in x, s will be in result iff
+    s matches at least one string in keep AND matches no strings in discard.
+    If  keep   is None, pretend s matches something in keep.
+    If discard is None, pretend s matches nothing in discard.    
+    
+    Examples
+    --------
+    strmatches(['hi','hello','okay','ohhello'], keep=['hi'])
+    >>> ['hi']
+    strmatches(['hi','hello','okay','ohhello'], discard=['*hell*'])
+    >>> ['hi', 'okay']
+    strmatches(['hi','hello','okay','ohhello'], keep=['*o*'])
+    >>> ['hello', 'okay', 'ohhello']
+    strmatches(['hi','hello','okay','ohhello'], keep=['*o*'], discard="*hell*")
+    >>> ['okay']
+    """
+    keep    =   [keep]  if  type(keep)  ==str else keep
+    discard = [discard] if type(discard)==str else discard
+    
+    result = []
+    ###Check matches with something in keep###
+    if keep is None:
+        result = list(x) #makes a copy of x
+    else: #keep is not None
+        for key in x:
+            for s in keep:
+                if strmatch(s, key):
+                    result += [key]
+                    break
+    ###Check matches with nothing in discard###
+    if discard is not None:
+        keepers = []
+        for key in result:
+            keeping = True
+            for s in discard:
+                if strmatch(s, key):
+                    keeping = False
+                    break
+            if keeping:
+                keepers += [key]
+        result = keepers
+    ###return###
+    return result
+    
+    
 """
 #Simple example of using a wrapper function to do something if error occurs,
 #   then propagate the error forward (without handling it).
