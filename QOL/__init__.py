4c1022c97e740a64422f5ddb48c11172e416b45e
Samuel Evans <Sevans@bu.edu>
2020-05-28 12:20:40 -0400

    Initial commit for __init__.py in new file structure

---
+++ QOL/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 12:12:21 2020
+
+@author: Sevans
+"""
+
+__all__ = ['codes', 'files', 'plots']
+
+from . import codes
+from . import files
+from . import plots
