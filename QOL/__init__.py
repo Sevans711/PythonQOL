2e6742976a5869a50f3f3bb22cef93cbb9638a3f
Samuel Evans <Sevans@bu.edu>
2020-09-24 16:17:31 -0400

    [ADD] puts QOL/animations in __init__.py

--- QOL/__init__.py
+++ QOL/__init__.py
@@ -6,8 +6,9 @@ Created on Thu May 28 12:12:21 2020
 @author: Sevans
 """
 
-__all__ = ['codes', 'files', 'plots']
+__all__ = ['codes', 'files', 'plots', 'animations']
 
 from . import codes
 from . import files
 from . import plots
+from . import animations
